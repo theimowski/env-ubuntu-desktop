d10eaf5969005e7867d9195438cc83d4fa90d747
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-01-07 15:10:37 +0100

    uninstall unnecessary apps

--- provision.sh
+++ provision.sh
@@ -1,3 +1,7 @@
 apt-get update
 apt-mark hold grub-pc
 apt-get -y upgrade
+apt-get remove -y --purge libreoffice-core \
+                          libreoffice-common \
+                          unity-webapps-common \
+                          ubuntu-software
