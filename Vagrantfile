ff1f71c4eb10558da48fa3131ed6a6f96aecca4d
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-01-08 08:47:37 +0100

    chrome

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,9 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "boxcutter/ubuntu1604-desktop"
   config.vm.provision "shell", path: "provision.sh"
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 4096
+    v.cpus = 2
+    v.customize ["setextradata", :id, "GUI/Fullscreen", "on"]
+  end
 end
--- provision.sh
+++ provision.sh
@@ -1,7 +1,12 @@
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
-apt-mark hold grub-pc
-apt-get -y upgrade
 apt-get remove -y --purge libreoffice-core \
                           libreoffice-common \
                           unity-webapps-common \
                           ubuntu-software
+apt-get -y install google-chrome-stable
+apt-mark hold grub-pc
+apt-get -y upgrade
+
+echo -e 'LANG="en_US.UTF-8"\nLANGUAGE="en_US"' | sudo tee /etc/default/locale > /dev/null
